67fc9e1589fdd079624c4ec0b2a716841524dc8c
David Todorovski <todorovskidavid23@gmail.com>
2026-03-06 20:53:01 +0100

    added

--- class13/scripts/modules/storageService.js
+++ class13/scripts/modules/storageService.js
@@ -5,3 +5,10 @@
 //CRUD CREATE READ UPDATE DELEA EDIT ADD
 
 //CREATE READ UPDATE DELETE CRUD operacii
+
+//sve sto raborime e so apito na localStorage
+//napravivme crud operacii
+//ni treba ghyml code za input fields for each popery from the UserClass isto taka a input field za id and 
+//prepopulated the fields withdata crud operation create read update delete
+
+ 
