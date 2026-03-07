becb1f901c703d653660b2fd158fd5d382e02e64
David <todorovskidavid23@gmail.com>
2026-03-07 19:10:44 +0100

    home update

---
+++ WorkShop/Countrypedia/Code/scripts/app.js
@@ -0,0 +1,10 @@
+// 1. Select all the html elements needed for the implemntation
+// 2. Implement function to fetch data from API (provide fields what kind of data do we need!)
+// 3. Implement function that will display/hide spinner
+// 4. Implement function that will display data in Cards
+// 5. Implement function that will display data in Table
+// 6. Implement 5 event listeners for the five buttons
+// 6.1 Search, Reset, All from Europe, All neigbours MKD, MKD
+// 7. Implement constructor function with props only needed for the data to be displayed
+
+let searchInput=document.getElementsByClassName("form-control")[0];
